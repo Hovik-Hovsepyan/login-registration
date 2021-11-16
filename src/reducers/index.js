95a0aa964a5f03877ed83772a63159d6135b8261
Davit Harut <davit@cincopa.com>
2021-11-16 21:49:41 +0400

    All reducers moved to combinedReducer

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import {IsLoadingReducer} from './IsLoadingReducer';
+import {UserLoggedReducer} from './UserLoggedReducer';
+import {SignUpDataCollector} from './SignUpDataCollector';
+
+export const combinedReducers = {
+  IsLoadingReducer,
+  UserLoggedReducer,
+  SignUpDataCollector,
+};
